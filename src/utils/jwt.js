dee9da7d12f1dcdbb1348f2717e2c9c9e194a2b7
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-04-27 12:25:55 +0200

    Plantilla puesta

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken")
 
 const generateToken = (data) => {
-    return jwt.sign(data, process.env.SECRET_JWT, { expiresIn: '2h' })
+    //data, informacion a guardar en el token
+    //secreteKeyPepino , clave secreta que me invento
+    //{ expiresIn: '1h' } tiempo de duracion del token
+    return jwt.sign(data, process.env.SECRET_JWT, { expiresIn: '1h' })
 }
 const verifyToken = (token) => {
     console.log(token)
-    return jwt.verify(token, "secreteKeyJoaquin")
+    return jwt.verify(token, "secreteKeyPepino")
 
 }
 
