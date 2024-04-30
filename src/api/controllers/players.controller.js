c9ebe5bd761febcb55cd48daa81c7e162842d187
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-04-30 12:51:08 +0200

    Plantilla completa

--- src/api/controllers/players.controller.js
+++ src/api/controllers/players.controller.js
@@ -1,7 +1,7 @@
 const Player = require('../models/players.model');
 const bcrypt = require('bcrypt');
 
-app.get('/players', async (req, res) => {
+  app.get('/players', async (req, res) => {
     const players = await Player.find();
     res.json(players);
   });
@@ -10,4 +10,7 @@ app.get('/players', async (req, res) => {
     const newPlayer = new Player(req.body);
     await newPlayer.save();
     res.json(newPlayer);
-  });
+  }
+
+
+);
